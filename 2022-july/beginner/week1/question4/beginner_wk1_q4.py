2da1f246188401997b6509b8f7042ad65e259ca7
Dippudo <107745404+dippudo@users.noreply.github.com>
2022-08-26 15:58:11 +1000

    Create beginner_wk1_q4.py

---
+++ 2022-july/beginner/week1/question4/beginner_wk1_q4.py
@@ -0,0 +1,11 @@
+line1 = 'If you are grumpy and you know it'
+line2 = 'Stomp your feet'
+
+print(line1)
+print(line2)
+print(line1)
+print(line2)
+print(line1)
+print('and you really want to show it')
+print(line1)
+print(line2)
