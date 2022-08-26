5f46bcf6b0b124946315b50a957b2ef77af41ef4
Dippudo <107745404+dippudo@users.noreply.github.com>
2022-08-26 16:10:40 +1000

    Create beginner_wk1_q8.py

---
+++ 2022-july/beginner/week1/question8/beginner_wk1_q8.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+inside_angle = 135
+turn = 180 - inside_angle
+
+
+forward(50)
+left(turn)
+forward(50)
+left(turn)
+forward(50)
+left(turn)
+forward(50)
+left(turn)
+forward(50)
+left(turn)
+forward(50)
+left(turn)
+forward(50)
+left(turn)
+forward(50)
