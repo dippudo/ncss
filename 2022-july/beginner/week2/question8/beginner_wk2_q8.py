1f79779f0699df820b551b326c728dcb7b900503
Dippudo <107745404+dippudo@users.noreply.github.com>
2022-08-28 12:05:05 +1000

    Create beginner_wk2_q8.py

---
+++ 2022-july/beginner/week2/question8/beginner_wk2_q8.py
@@ -0,0 +1,28 @@
+from turtle import *
+colour = input("Colour? ")
+
+pencolor(colour)
+pensize(3)
+
+left(60)
+forward(60)
+right(120)
+forward(60)
+left(60)
+
+penup()
+forward(10)
+
+pendown()
+forward(30)
+
+penup()
+forward(10)
+
+pendown()
+
+left(60)
+forward(60)
+right(120)
+forward(60)
+left(60)
